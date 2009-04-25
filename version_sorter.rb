c23604ae0c190203d3b278df33361ed15604e73e
Chris Wanstrath <chris@ozmm.org>
2009-04-24 17:19:18 -0700

    use rsort if you want a reverse sorting

--- version_sorter.rb
+++ version_sorter.rb
@@ -3,6 +3,10 @@ module VersionSorter
   extend self
 
   def sort(list)
+    list.sort { |a, b| versioncmp(a, b) }
+  end
+
+  def rsort(list)
     list.sort { |a, b| -versioncmp(a, b) }
   end
 
@@ -47,9 +51,16 @@ if $0 == __FILE__
 
     def test_sorts_verisons_correctly
       versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
-      sorted_versions = %w( 3.1.4.2 2.0 1.0.10 1.0.9a 1.0.9 )
+      sorted_versions = %w( 1.0.9 1.0.9a 1.0.10 2.0 3.1.4.2 )
 
       assert_equal sorted_versions, sort(versions)
+    end
+
+    def test_reverse_sorts_verisons_correctly
+      versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
+      sorted_versions = %w( 3.1.4.2 2.0 1.0.10 1.0.9a 1.0.9 )
+
+      assert_equal sorted_versions, rsort(versions)
     end
   end
 end
