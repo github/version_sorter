5ab827ef5c78f9d3dddfa883e5d6dd6c25515c26
K. Adam Christensen <pope@shifteleven.com>
2009-10-12 00:04:32 -0400

    Reorganizing the Directory Structure.
    I removed the xcode stuff and put everything that was in src into the
    ext directory.
    
    I also updated the code of version_sorter.rb to use the C extension.

--- version_sorter.rb
+++ version_sorter.rb
@@ -1,59 +1,29 @@
-require 'ffi'
-require 'pp'
+require 'test/unit'
+require 'ext/version_sorter'
 
-module VersionSorter
-  extend FFI::Library
+class VersionSorterTest < Test::Unit::TestCase
+  include VersionSorter
 
-  ffi_lib "ext/version_sorter/build/Debug/libversion_sorter.dylib"
-  attach_function :version_sorter_sort, [:pointer, :int], :void
+  def test_sorts_verisons_correctly
+    versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
+    sorted_versions = %w( 1.0.9 1.0.9a 1.0.10 2.0 3.1.4.2 )
 
-  extend self
-
-  def sort(list)
-
-    strptrs = list.map { |x| FFI::MemoryPointer.from_string(x) }
-
-    argv = FFI::MemoryPointer.new(:pointer, list.size)
-    argv.write_array_of_pointer(strptrs)
-
-    version_sorter_sort(argv, list.size)
-
-    argv.read_array_of_pointer(list.size).map { |x| x.get_string(0) }
+    assert_equal sorted_versions, sort(versions)
   end
 
-  def rsort(list)
-    sort(list).reverse
+  def test_reverse_sorts_verisons_correctly
+    versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
+    sorted_versions = %w( 3.1.4.2 2.0 1.0.10 1.0.9a 1.0.9 )
+
+    assert_equal sorted_versions, rsort(versions)
   end
 end
 
-if $0 == __FILE__
-  require 'test/unit'
-
-  class VersionSorterTest < Test::Unit::TestCase
-    include VersionSorter
-
-    def test_sorts_verisons_correctly
-      versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
-      sorted_versions = %w( 1.0.9 1.0.9a 1.0.10 2.0 3.1.4.2 )
-
-      assert_equal sorted_versions, sort(versions)
-    end
-
-    def test_reverse_sorts_verisons_correctly
-      versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
-      sorted_versions = %w( 3.1.4.2 2.0 1.0.10 1.0.9a 1.0.9 )
-
-      assert_equal sorted_versions, rsort(versions)
-    end
-  end
-
-  require 'benchmark'
-  versions = IO.read('tags.txt').split("\n")
-  count = 10
-  Benchmark.bm(20) do |x|
-    x.report("sort")             { count.times { VersionSorter.sort(versions) } }
-    x.report("rsot")             { count.times { VersionSorter.rsort(versions) } }
-  end
-  puts
-
+require 'benchmark'
+versions = IO.read('tags.txt').split("\n")
+count = 10
+Benchmark.bm(20) do |x|
+  x.report("sort")             { count.times { VersionSorter.sort(versions) } }
+  x.report("rsot")             { count.times { VersionSorter.rsort(versions) } }
 end
+puts
