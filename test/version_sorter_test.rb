1a82ab1a24c8ce7e1c8c10e6c7538abb4ddd8f97
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-02-03 20:16:47 -0800

    Avoid including VersionSorter as mixin
    Tests should use the module as it's meant to be used in the wild.

--- test/version_sorter_test.rb
+++ test/version_sorter_test.rb
@@ -2,18 +2,16 @@ require 'test/unit'
 require 'version_sorter'
 
 class VersionSorterTest < Test::Unit::TestCase
-  include VersionSorter
-
   def test_sorts_versions_correctly
     versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
     sorted_versions = %w( 1.0.9 1.0.9a 1.0.10 2.0 3.1.4.2 )
 
-    assert_equal sorted_versions, sort(versions)
+    assert_equal sorted_versions, VersionSorter.sort(versions)
   end
 
   def test_returns_same_object
     versions = %w( 2.0 1.0 0.5 )
-    sorted = sort(versions)
+    sorted = VersionSorter.sort(versions)
 
     assert_equal versions[2].object_id, sorted[0].object_id
   end
@@ -22,6 +20,6 @@ class VersionSorterTest < Test::Unit::TestCase
     versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
     sorted_versions = %w( 3.1.4.2 2.0 1.0.10 1.0.9a 1.0.9 )
 
-    assert_equal sorted_versions, rsort(versions)
+    assert_equal sorted_versions, VersionSorter.rsort(versions)
   end
 end
