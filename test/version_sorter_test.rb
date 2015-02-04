602697e675ec72f467cbb68427acffe219751d5b
Mislav Marohnić <mislav.marohnic@gmail.com>
2015-02-03 20:14:32 -0800

    Typos in tests

--- test/version_sorter_test.rb
+++ test/version_sorter_test.rb
@@ -4,7 +4,7 @@ require 'version_sorter'
 class VersionSorterTest < Test::Unit::TestCase
   include VersionSorter
 
-  def test_sorts_verisons_correctly
+  def test_sorts_versions_correctly
     versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
     sorted_versions = %w( 1.0.9 1.0.9a 1.0.10 2.0 3.1.4.2 )
 
@@ -18,7 +18,7 @@ class VersionSorterTest < Test::Unit::TestCase
     assert_equal versions[2].object_id, sorted[0].object_id
   end
 
-  def test_reverse_sorts_verisons_correctly
+  def test_reverse_sorts_versions_correctly
     versions = %w(1.0.9 1.0.10 2.0 3.1.4.2 1.0.9a)
     sorted_versions = %w( 3.1.4.2 2.0 1.0.10 1.0.9a 1.0.9 )
 
